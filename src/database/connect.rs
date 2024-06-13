c8ea59ea62f18915433dddb6f1c895a669c35c91
Aradhya Tripathi <67282231+Aradhya-Tripathi@users.noreply.github.com>
2024-06-13 12:04:57 +0100

    refactor: simplified connect

--- src/database/connect.rs
+++ src/database/connect.rs
@@ -2,6 +2,7 @@ use mysql::Pool;
 use mysql::PooledConn;
 use regex::Regex;
 use rusqlite::Connection as SqliteConnection;
+use std::fmt;
 use std::path::Path;
 
 const URI_PATTERN: &str = r"^mysql:\/\/([^:\/?#]+):([^@\/?#]+)@([^:\/?#]+):(\d+)\/([^\/?#]+)$";
@@ -10,6 +11,12 @@ pub struct Database {
     pub flavour: SupportedDatabases,
     // We are taking control of the connection at this point
     pub connection: ConnectionTypes,
+}
+
+impl fmt::Display for Database {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Connected to {:?}", self.flavour)
+    }
 }
 
 #[derive(Debug)]
@@ -34,38 +41,26 @@ impl SupportedDatabases {
 }
 
 impl Database {
-    pub fn new(path: &str, flavour: &str) -> Result<Self, String> {
+    pub fn new(path: &str) -> Result<Self, String> {
         let processed_path = path.trim_end();
-        match SupportedDatabases::from_string(flavour) {
-            SupportedDatabases::MySQL => {
-                if Regex::new(URI_PATTERN)
-                    .unwrap()
-                    .captures(processed_path)
-                    .is_some()
-                {
-                    let connection_pool: PooledConn =
-                        Pool::new(processed_path).unwrap().get_conn().unwrap();
-                    return Ok(Database {
-                        flavour: SupportedDatabases::MySQL,
-                        connection: ConnectionTypes::MySQL(connection_pool),
-                    });
-                }
-                Err(format!("Invalid connection string: {path}"))
-            }
-            SupportedDatabases::SQLite => match Path::try_exists(Path::new(processed_path)) {
-                Ok(found) => {
-                    if found {
-                        let connection: SqliteConnection =
-                            SqliteConnection::open(processed_path).unwrap();
-                        return Ok(Database {
-                            flavour: SupportedDatabases::SQLite,
-                            connection: ConnectionTypes::SQLite(connection),
-                        });
-                    }
-                    Err(format!("Failed to find in-memory database {path}"))
-                }
-                Err(_) => Err(format!("Error occured while looking for database {path}")),
-            },
+        if Path::exists(Path::new(processed_path)) {
+            let connection: SqliteConnection = SqliteConnection::open(processed_path).unwrap();
+            return Ok(Database {
+                flavour: SupportedDatabases::SQLite,
+                connection: ConnectionTypes::SQLite(connection),
+            });
+        } else if Regex::new(URI_PATTERN)
+            .unwrap()
+            .captures(processed_path)
+            .is_some()
+        {
+            let connection_pool: PooledConn =
+                Pool::new(processed_path).unwrap().get_conn().unwrap();
+            return Ok(Database {
+                flavour: SupportedDatabases::MySQL,
+                connection: ConnectionTypes::MySQL(connection_pool),
+            });
         }
+        Err(format!("Failed to process database URI: {processed_path}"))
     }
 }
