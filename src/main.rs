b2b3b0303a0eacd0c67f77c150a07c9b790346cb
Aradhya Tripathi <67282231+Aradhya-Tripathi@users.noreply.github.com>
2024-06-15 14:32:55 +0100

    wip: bringing it all together

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+/*
+All these configurations will later be stored somewhere
+Once I decide on the interface I want to use to interact with this
+for now on boot all fields need to filled.
+We are also analyzing the query before running it to disallow
+unwanted query runs (which will be implemented later right now we have access to everything)
+*/
+
 use diffpriv::database::database::Database;
 use diffpriv::database::schema::{Column, Schema};
 use diffpriv::query::analyzer;
+use diffpriv::transforms::transform;
 
+use std::collections::HashMap;
 use std::io::{self, Write};
-
-/// All these configurations will later be stored somewhere
-/// Once I decide on the interface I want to use to interact with this
-/// for now on boot all fields need to filled.
 
 fn used_columns(requested: Vec<String>, mut existing: Vec<Column>) -> Vec<Column> {
     let mut used_columns: Vec<Column> = vec![];
@@ -21,7 +27,7 @@ fn used_columns(requested: Vec<String>, mut existing: Vec<Column>) -> Vec<Column
     while index < existing.len() {
         for func in aggregate_functions.iter() {
             if requested.contains(&format!("{func}{})", existing[index].name)) {
-                existing[index].usage = Some(func.replace("(", ""));
+                existing[index].usage = Some(format!("{func}{})", existing[index].name));
                 used_columns.push(existing[index].to_owned());
             }
         }
@@ -35,7 +41,38 @@ fn used_columns(requested: Vec<String>, mut existing: Vec<Column>) -> Vec<Column
     used_columns
 }
 
-pub fn main() {
+fn apply_transforms(
+    used_columns: Vec<Column>,
+    query_result: Vec<HashMap<String, String>>,
+) -> Vec<f64> {
+    // Test function!
+    // If usage does not exist that means this query is not trying to get the
+    // average of a perticular row, instead it's query the whole row or something else
+    // which in any case is not allowed!
+    let mut transformed_results: Vec<f64> = vec![];
+
+    for result in query_result {
+        for (k, v) in result {
+            let found_column = used_columns
+                .iter()
+                .find(|column| {
+                    column
+                        .usage
+                        .to_owned()
+                        .expect("Illegal usage no aggregate used on this column!")
+                    // This will later be removed and we will have
+                    // A strict query checker before the query is actually executed!
+                        == k
+                })
+                .unwrap();
+            let true_value = v.parse::<f64>().unwrap(); // This will always work since we are only allowing aggregate queries
+            transformed_results.push(transform(true_value, found_column.sensitivity));
+        }
+    }
+    transformed_results
+}
+
+fn main() {
     println!("------CONFIGURATION-------");
     print!("Database Path/URI> ");
     io::stdout().flush().unwrap();
@@ -79,8 +116,7 @@ pub fn main() {
                 .flat_map(|table| table.columns.clone())
                 .collect(),
         );
-        let results = database_connection.execute_query(&query);
-        println!("Used columns: {used_columns:?}");
-        println!("Query results: {results:?}");
+        let query_result = database_connection.execute_query(&query);
+        println!("{:?}", apply_transforms(used_columns, query_result));
     }
 }
---
+++ src/transforms/mod.rs
@@ -0,0 +1,19 @@
+use rand::Rng;
+
+fn laplace_sample(location: f64, scale: f64) -> f64 {
+    let mut rng = rand::thread_rng();
+    let u: f64 = rng.gen_range(-0.5..0.5);
+    location - scale * u.signum() * (1.0 - 2.0 * u.abs()).ln() // Formula for laplace sample
+}
+
+fn add_laplace_noise(true_value: f64, sensitivity: f64, epsilon: f64) -> f64 {
+    let noise = laplace_sample(0.0, sensitivity / epsilon);
+    true_value + noise
+}
+
+pub fn transform(true_value: f64, sensitivity: f64) -> f64 {
+    let epsilon = 0.1; // Privacy Budget - User defined however for now we just define it to test things out
+
+    let noisy = add_laplace_noise(true_value, sensitivity, epsilon);
+    noisy
+}
