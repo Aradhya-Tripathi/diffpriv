c9500fc573b9e7741b7787e387f4ae997ab8907a
Aradhya Tripathi <67282231+Aradhya-Tripathi@users.noreply.github.com>
2024-06-22 22:25:38 +0100

    feat: styles

--- src/components/ConfigForm.jsx
+++ src/components/ConfigForm.jsx
@@ -7,15 +7,17 @@ const ConfigForm = ({ onConnect }) => {
   const [databasePath, setDatabasePath] = useState("");
 
   const configure = async () => {
-    if (databasePath) {
+    if (databasePath.trim()) {
       try {
         const msg = await invoke("connect", { databasePath });
         setDatabasePath("");
         toast.success(msg, { duration: 2000 });
         onConnect();
       } catch (err) {
-        toast.error(err, { duration: 2000 });
+        toast.error(err.message, { duration: 2000 });
       }
+    } else {
+      toast.error("Database path cannot be empty", { duration: 2000 });
     }
   };
 
@@ -28,12 +30,13 @@ const ConfigForm = ({ onConnect }) => {
     <div className="config-form-container" id="config-form">
       <form onSubmit={handleSubmit}>
         <div className="form-group">
+          <label htmlFor="configPath">Database Path/URI</label>
           <input
             type="text"
             id="configPath"
             value={databasePath}
             onChange={(e) => setDatabasePath(e.target.value)}
-            placeholder="Path/URI"
+            placeholder="Enter Path/URI"
             required
           />
         </div>
